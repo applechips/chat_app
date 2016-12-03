45e672f457e779d38bafb01d6706c4286ea574db
Nicole <nhswong@gmail.com>
2016-12-02 20:41:27 -0800

    add action cable to client & server side

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -19,8 +19,9 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
-  
+
 
   private
 
